28d676241ac8572e458d9a73284ec27e2d37b7f3
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-09-25 10:10:43 -0700

    Add logic to load username

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 const confirmPasswordInput = document.getElementById('confirmPassword');
 
+// load username, remember to us 'savedUsername' when saving it later
+window.addEventListener('load', () => {
+    const savedUsername = sessionStorage.getItem('savedUsername');
+    if (savedUsername) {
+        usernameInput.value = savedUsername;
+    }
+});
+
+
+
