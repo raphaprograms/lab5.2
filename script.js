a06cc11e015f209ed4ccbed38c49c15f5b1c5d26
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-09-25 11:21:17 -0700

    Add functions to check input validity

--- script.js
+++ script.js
@@ -10,7 +10,46 @@ window.addEventListener('load', () => {
     if (savedUsername) {
         usernameInput.value = savedUsername;
     }
+    
 });
 
+function validateUsername(input) {
+    if(!input.validity.valid){
+        if (input.value.length < 5) {
+            input.setCustomValidity('Username must be 5 characters or longer.');
+        } else {
+            input.setCustomValidity('');
+        }
+    }
+}
 
+function validateEmail(input) {
+    if(!input.validity.valid){
+        if(input.validity.valueMissing){
+            input.setCustomValidity('Email is required');
+        } else if (input.validity.typeMismatch) {
+            input.setCustomValidity('Please enter a valid email address.');
+        }
+    }
+    // optionally use built in html email validation and substitude in code below
+    // input.setCustomValidity('');
+}
+
+function validatePassword(input) {
+    if(!input.validity.valid){
+        if (input.value.length < 8) {
+            input.setCustomValidity('Password must be at least 8 characters');
+        } else {
+            input.setCustomValidity('');
+        }
+    }
+}
+
+function validateConfirmPassword(input) {
+    if (input.value !== passwordInput.value) {
+        input.setCustomValidity('Passwords do not match!');
+    } else {
+        input.setCustomValidity('');
+    }
+}
 
