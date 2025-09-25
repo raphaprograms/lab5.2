d2f98c2cb9f4da47c2d0ba1b1c248ef045129995
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-09-25 10:03:49 -0700

    Add given html and css and select DOM input elements

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('registrationForm');
+const usernameInput = document.getElementById('username');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const confirmPasswordInput = document.getElementById('confirmPassword');
+
